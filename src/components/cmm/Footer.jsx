5a6ac6ddeaeeeae3259045177326c3d7a66aac59
kmee <devKmee@gmail.com>
2026-07-23 19:44:12 +0900

    FEAT: 푸터 수정

--- src/components/cmm/Footer.jsx
+++ src/components/cmm/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
     return (
         <footer style={{ display: 'flex', gap: '10px' }}>
@@ -7,28 +9,32 @@ function Footer() {
                     flex: 1,
                     backgroundColor: '#ffb6d9',
                     display: 'flex',
-                    width: '400px',
+                    width: '4px',
                     height: '150px',
                     justifyContent: 'center',
                     alignItems: 'center',
+                    cursor: 'pointer',
                 }}
             >
                 <p>&copy; 클라이언트 요청링크</p>
             </div>
-            <div
-                className="footer-content"
-                style={{
-                    flex: 1,
-                    width: '400px',
-                    height: '150px',
-                    backgroundColor: '#e6d7f0',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <p>&copy; madeby 크레페 홍보링크</p>
-            </div>
+            <Link to="https://crepe.cm/ko" style={{ textDecoration: 'none' }}>
+                <div
+                    className="footer-content"
+                    style={{
+                        flex: 1,
+                        width: '450px',
+                        height: '150px',
+                        backgroundColor: '#e6d7f0',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        cursor: 'pointer',
+                    }}
+                >
+                    <p>&copy; madeby 크레페 홍보링크</p>
+                </div>
+            </Link>
         </footer>
     );
 }
